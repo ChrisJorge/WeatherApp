d4c901753ac0b750a39609745226284b82ea8700
Chris <Christopherjorge47@icloud.com>
2024-05-19 21:01:55 -0400

    Put data onto the screen

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Input from './Input'
 function Homepage({api}) {
     const [text, updateText] = useState('')
+    const [weather, setWeather] = useState(null)
 
     const getData = async () => {
         let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${text}?key=${api}`
@@ -10,20 +11,50 @@ function Homepage({api}) {
             const data = await fetch(url);
             const response = await data.json()
             console.log(response)
+            setWeather(response)
         }
         catch (e)
         {
             console.error(e)
         }
     }
-  return (
-    <>
-        <div className="inputContainer">
-            <Input type={'text'} text={'Enter Location'} state = {text} setState={updateText}/>
-            <button onClick={getData}>Submit</button>
-        </div>    
-    </>
-  )
+    const loading = () => {
+
+        return (
+            <>
+                <div className="inputContainer">
+                    <Input type={'text'} text={'Enter Location'} state = {text} setState={updateText}/>
+                    <button onClick={getData}>Submit</button>
+                </div>    
+            </>
+          )
+    } 
+
+    const loaded = () => {
+        return(
+            <>
+            <div className="inputContainer">
+                     <Input type={'text'} text={'Enter Location'} state = {text} setState={updateText}/>
+                     <button onClick={getData}>Submit</button>
+             </div>
+             <div className="information">
+                <h1>{weather.resolvedAddress}</h1>
+                <h3>Todays conditions: {weather.days[0].conditions}</h3>
+                <h3>Max Temperature: {weather.days[0].tempmax} </h3>
+                <h3>Min Temperature: {weather.days[0].tempmin} </h3>
+                <h3>Humidity: {weather.days[0].humidity} </h3>
+
+                
+             </div>
+             
+         </>
+        )
+       
+    }
+       
+    
+
+  return weather ? loaded(): loading();
 }
 
 export default Homepage
